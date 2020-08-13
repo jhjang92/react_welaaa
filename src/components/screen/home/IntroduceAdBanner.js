80976c18838ca8ba7dea30e2b68502cc824c7aeb
jaehyuk Jang <jhjang92@kakao.com>
2020-08-13 16:29:47 +0900

    feature: adBanner css 작업완료
    link는실제 사이트의 주소를 사용하였습니다.
    Ad banner css #12

--- src/components/screen/home/IntroduceAdBanner.js
+++ src/components/screen/home/IntroduceAdBanner.js
@@ -1,13 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 function IntroduceAdBanner() {
+  const AdBannerBox = styled.div`
+    cursor: pointer;
+    a {
+      img {
+        width: 100%;
+      }
+    }
+  `;
   return (
-    <section className="section_banner-ad">
-      <img
-        src="images/static/banner-web/오디오북카드지갑_1920x150.jpg"
-        alt="오디오북카드지갑"
-      ></img>
-    </section>
+    <AdBannerBox className="banner-ad">
+      <a
+        href="https://smartstore.naver.com/welaaa/category/3b13e5e6f3e14a19b32a887bcaf0ba65?cp=1"
+        target="_blank"
+      >
+        <img
+          src="images/static/banner-web/오디오북카드지갑_1920x150.jpg"
+          alt="오디오북카드지갑"
+        ></img>
+      </a>
+    </AdBannerBox>
   );
 }
 
