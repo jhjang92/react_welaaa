e7a6733873ee99f28bcd1af9869900ca13ef2254
jaehyuk Jang <jhjang92@kakao.com>
2020-08-13 12:31:01 +0900

    feature : bookbox css 끝
    bookbox css 입히기 끝 a태그의 경로들은마지막에 다시 잡아줘야합니다.
    css bookbox #8

--- src/components/screen/home/IntroduceBookBox.js
+++ src/components/screen/home/IntroduceBookBox.js
@@ -1,8 +1,140 @@
 import React from "react";
+import styled from "styled-components";
 
 function IntroduceBookBox() {
+  const SectionBookBox = styled.section`
+    padding-top: 28px;
+    box-sizing: border-box;
+    text-align: center;
+    header {
+      width: 1024px;
+      margin: 0 auto;
+      display: flex;
+      flex-direction: column-reverse;
+      align-items: center;
+      margin-bottom: 100px;
+      h3 {
+        height: 88px;
+        margin: 0;
+        img {
+          height: 100%;
+        }
+      }
+      p {
+        font-size: 30px;
+        margin: 0;
+        margin-bottom: 12px;
+        :last-of-type {
+          font-size: 20px;
+          margin-bottom: 10px;
+        }
+        strong {
+          font-weight: 500;
+          color: #030303;
+        }
+      }
+    }
+  `;
+  const FlexUlBox = styled.ul`
+    display: flex;
+    justify-content: center;
+    li {
+      width: 234px;
+      box-sizing: border-box;
+      margin-right: 60px;
+
+      position: relative;
+      :last-of-type {
+        margin-right: 0;
+      }
+      ::after {
+        content: "";
+        width: 234px;
+        height: 234px;
+        display: block;
+        position: absolute;
+        top: 35%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: -1;
+        background: #000;
+        border-radius: 50%;
+      }
+      a {
+        display: block;
+      }
+      :hover {
+        ::before {
+          content: "";
+          width: 74px;
+          height: 190px;
+          background: url(images/introduce/bookbox_record.png);
+          display: block;
+          position: absolute;
+          top: -12px;
+          right: -17px;
+          z-index: 1;
+        }
+      }
+    }
+  `;
+  const Figure = styled.figure`
+    margin: 0 auto;
+    text-align: center;
+    img {
+      width: 162px;
+      height: 244px;
+      margin: 0 auto;
+      background-color: #efefef;
+      border-radius: 10px;
+      display: block;
+      box-shadow: 0 7px 7px 0 rgba(0, 0, 0, 0.2);
+
+      filter: grayscale(40%);
+    }
+    figcaption {
+      p {
+        margin: 0;
+        font-size: 18px;
+        font-weight: 200;
+        letter-spacing: -2px;
+        word-break: keep-all;
+
+        :first-of-type {
+          color: #030303;
+        }
+      }
+    }
+    :hover {
+      img {
+        filter: grayscale(0%);
+      }
+      figcaption {
+        p {
+          :first-of-type {
+            font-weight: 500 !important;
+          }
+        }
+      }
+    }
+  `;
+  const LinkFreeListen = styled.a`
+    button {
+      cursor: pointer;
+      margin: 72px auto 134px;
+      font-size: 18px;
+      padding: 12px 45px;
+      box-sizing: border-box;
+      font-weight: 500;
+      background-color: #f5f5f5;
+      border: 1px solid #e7e7e7;
+      color: #030303;
+      letter-spacing: -2px;
+      line-height: 1;
+    }
+  `;
   return (
-    <section className="section_bookbox-audiobook">
+    <SectionBookBox className="section_bookbox-audiobook">
       <header>
         <h3>
           <img src="images/introduce/bookbox_title.png" alt="북크박스"></img>
@@ -12,10 +144,10 @@ function IntroduceBookBox() {
         </p>
         <p>지식인들의 10분 북인사이트</p>
       </header>
-      <ul>
+      <FlexUlBox>
         <li>
           <a href="#">
-            <figure>
+            <Figure>
               <img
                 src="images/introduce/8_1.png"
                 alt="걸어서 출퇴근하는 배우"
@@ -24,42 +156,36 @@ function IntroduceBookBox() {
                 <p>걷는사람,하정우</p>
                 <p>하정우</p>
               </figcaption>
-            </figure>
+            </Figure>
           </a>
         </li>
         <li>
           <a href="#">
-            <figure>
-              <img
-                src="images/introduce/8_1.png"
-                alt="걸어서 출퇴근하는 배우"
-              ></img>
+            <Figure>
+              <img src="images/introduce/8_2.png" alt=""></img>
               <figcaption>
-                <p>걷는사람,하정우</p>
-                <p>하정우</p>
+                <p>맥파이 살인사건</p>
+                <p>엔터니호로비츠</p>
               </figcaption>
-            </figure>
+            </Figure>
           </a>
         </li>
         <li>
           <a href="#">
-            <figure>
-              <img
-                src="images/introduce/8_1.png"
-                alt="걸어서 출퇴근하는 배우"
-              ></img>
+            <Figure>
+              <img src="images/introduce/8_3.png" alt=""></img>
               <figcaption>
-                <p>걷는사람,하정우</p>
-                <p>하정우</p>
+                <p>스물아홉 생일, 1년 후 죽기로 결심했다</p>
+                <p>하야마 아마리</p>
               </figcaption>
-            </figure>
+            </Figure>
           </a>
         </li>
-      </ul>
-      <a href="#">
+      </FlexUlBox>
+      <LinkFreeListen href="#">
         <button type="button">무료듣기</button>
-      </a>
-    </section>
+      </LinkFreeListen>
+    </SectionBookBox>
   );
 }
 export default IntroduceBookBox;
